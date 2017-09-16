4dcfb26c9e4976dd9e610b5b4ea8912771939c08
Tihomir Car <tcar121293@gmail.com>
2017-09-16 17:55:10 +0200

    ready

--- server/auth/credentials.js
+++ server/auth/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
     appId:'118880012110829',
     appSecret:'5ee7d2cf7fc84bdff66109c41eb49b1f',
-    callback:process.env.HOST + '/auth/facebook/callback'
+    callback:process.env.HOST + 'auth/facebook/callback'
 
 }
